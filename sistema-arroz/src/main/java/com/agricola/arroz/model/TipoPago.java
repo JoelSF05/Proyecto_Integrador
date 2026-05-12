8828b28ca1085744d703e3b629ac182d429f2ebe
JoelSF05 <joel.simonfranco@gmail.com>
2026-05-11 21:14:12 -0500

    Iniciacion del proyecto

--- sistema-arroz/src/main/java/com/agricola/arroz/model/TipoPago.java
+++
@@ -1,5 +0,0 @@
-package com.agricola.arroz.model;
-
-public enum TipoPago {
-    tiempo, rendimiento
-}
---
+++ src/main/java/com/agricola/arroz/model/TipoPago.java
@@ -0,0 +1,5 @@
+package com.agricola.arroz.model;
+
+public enum TipoPago {
+    tiempo, rendimiento
+}
